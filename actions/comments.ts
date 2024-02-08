81ea92b6eb80be5e9def88b2621154d881b01740
Florin Camarut <florin.camarut@student.usv.ro>
2024-02-08 05:23:48 +0200

    removed notifications to test if it works without them on my deployment

--- actions/comments.ts
+++ actions/comments.ts
@@ -10,36 +10,46 @@ export const comments = async (body: string, postId: string) => {
   if (!postId || typeof postId !== 'string') {
     return { error: 'Invalid Id!' };
   }
+  try {
+    await db.comment.create({
+      data: {
+        body,
+        userId: currentUser?.id as string,
+        postId,
+      },
+    });
+    // try {
+    //   const post = await db.post.findUnique({
+    //     where: {
+    //       id: postId,
+    //     },
+    //   });
 
-  await db.comment.create({
-    data: {
-      body,
-      userId: currentUser?.id as string,
-      postId,
-    },
-  });
+    //   if (post?.userId) {
+    //     await prisma?.notification.create({
+    //       data: {
+    //         body: 'Someone replied to your tweet!',
+    //         userId: post.userId,
+    //       },
+    //     });
 
-  const post = await db.post.findUnique({
-    where: {
-      id: postId,
-    },
-  });
+    //     await db.user.update({
+    //       where: {
+    //         id: post.userId,
+    //       },
+    //       data: {
+    //         hasNotification: true,
+    //       },
+    //     });
+    //   }
+    // } catch (error) {
+    //   console.log(error);
+    //   return { error: 'Cannot post notification!' };
+    // }
 
-  await prisma?.notification.create({
-    data: {
-      body: 'Someone replied to your tweet!',
-      userId: post?.userId as string,
-    },
-  });
-
-  await db.user.update({
-    where: {
-      id: post?.userId as string,
-    },
-    data: {
-      hasNotification: true,
-    },
-  });
-
-  return { success: 'Succesfully added comment!' };
+    return { success: 'Succesfully added comment!' };
+  } catch (error) {
+    console.log(error);
+    return { error: 'Could not add a new comment!' };
+  }
 };
