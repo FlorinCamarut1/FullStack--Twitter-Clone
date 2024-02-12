c6c731282a485db80c714d756913ab7e07ad8726
Florin Camarut <florin.camarut@student.usv.ro>
2024-02-12 04:57:50 +0200

    i removed old modal for delete and used existing one , i added logic to delete specific posts

---
+++ actions/deleteComment.ts
@@ -0,0 +1,29 @@
+'use server';
+
+import { auth } from '@/auth';
+import usePosts from '@/hooks/usePosts';
+
+import db from '@/lib/db';
+
+export const deleteComment = async (commentId: string) => {
+  const session = await auth();
+  const currentUser = session?.user;
+
+  if (!commentId) return { error: 'Invalid Comment ID!' };
+
+  const comment = await db.comment.findUnique({
+    where: {
+      id: commentId,
+    },
+  });
+
+  if (currentUser?.id === comment?.userId) {
+    await db.comment.delete({
+      where: {
+        id: commentId,
+      },
+    });
+  }
+
+  return { success: 'Comment Deleted Succesfully!' };
+};
--- hooks/useUsers.ts
+++ hooks/useUsers.ts
@@ -3,7 +3,9 @@ import useSWR from 'swr';
 import fetcher from '@/lib/fetcher';
 
 const useUsers = () => {
-  const { data, error, isLoading, mutate } = useSWR('/api/users', fetcher);
+  const { data, error, isLoading, mutate } = useSWR('/api/users', fetcher, {
+    refreshInterval: 300_000,
+  });
 
   return { data, error, isLoading, mutate };
 };
